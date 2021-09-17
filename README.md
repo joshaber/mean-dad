7e5e28dd3c9298491cf4c20ccd557f5a6e391c11
joshaber <joshaber@gmail.com>
2021-09-17 08:54:19 -0400

    stealing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# simple-chores
+# Haikus for June
+
+A basic node / postgres app using an ejs template and a few API endpoints.
+
+Readme updates in progress!
---
+++ index.js
@@ -0,0 +1,36 @@
+let express = require('express');
+let app = express();
+let ejs = require('ejs');
+const haikus = require('./haikus.json');
+const port = process.env.PORT || 3000;
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.HOST,
+  port: 5432
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+console.log('hi');
+
+app.get('/', (req, res) => {
+  pool.query('SELECT * FROM haikus ORDER BY id', (err, haikus) => {
+    res.render('index', {haikus: haikus.rows});
+  });
+});
+
+app.post('/heart', (req, res) => {
+  pool.query('UPDATE haikus SET hearts = hearts + 1 WHERE id = $1', [req.body.id], (err, haikus) => {
+    res.send('Success');
+  });
+});
+
+app.listen(port);
+console.log('Server running on','https://localhost:3000')
